3d71f1b1584442f8052a882053a01a153db6c542
DIGYSKY <lilyan.chauveau@gmail.com>
2024-01-15 16:57:28 +0100

    Update Eethanol.py

--- Eethanol.py
+++ Eethanol.py
@@ -2,6 +2,14 @@
 # By Lilyan CHAUVEAU 
 # 15/01/2024
 
+# GNU GENERAL PUBLIC LICENSE
+
+#                        Version 3, 29 June 2007
+
+# Copyright (C) 2007 Free Software Foundation, Inc. <https://fsf.org/>
+# Everyone is permitted to copy and distribute verbatim copies
+# of this license document, but changing it is not allowed.
+
 import math
 
 def pression_carburant(ethanol_percentage=None, pression=None):
