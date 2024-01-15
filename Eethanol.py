68b535b72b69b468f87c3df4156874a7dca8f7a6
DIGYSKY <lilyan.chauveau@gmail.com>
2024-01-15 17:12:54 +0100

    Update Eethanol.py

--- Eethanol.py
+++ Eethanol.py
@@ -9,6 +9,7 @@
 # Copyright (C) 2007 Free Software Foundation, Inc. <https://fsf.org/>
 # Everyone is permitted to copy and distribute verbatim copies
 # of this license document, but changing it is not allowed.
+# See the LICENSE file
 
 import math
 
